e0c847ca52cc2912aa8ad409505669067d4dc2e7
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2024-04-22 23:05:11 +0530

    done

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -4,12 +4,33 @@ import CoinCard from "./CoinCard";
 function CoinDisplay({ coin }) {
   const [coins, setCoins] = useState([]);
 
+  async function getTopCoinsInfo() {
+    const response = await fetch("https://api.coinlore.net/api/tickers/");
+    const coinsData = await response.json();
+    // console.log(coinsData);
+    const top20Coins = await coinsData.data.slice(0, 20);
+    console.log(top20Coins);
+    setCoins(top20Coins);
+  }
+
+  useEffect(() => {
+    getTopCoinsInfo();
+  }, []);
+
   return (
     <div className="home">
       <h1>Top 20 Cryptos</h1>
       <div className="coins-container">
-        {coins.map((coin) => (
-          <CoinCard />
+        {/* {coins.map((coin) => ( */}
+        {coins.map(({ id, name, symbol, rank, price_usd }) => (
+          <CoinCard
+            id={id}
+            name={name}
+            symbol={symbol}
+            rank={rank}
+            price={price_usd}
+            key={id}
+          />
         ))}
       </div>
     </div>
--- pages/coins/[coin_id].js
+++ pages/coins/[coin_id].js
@@ -1,20 +1,45 @@
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 function CoinDetail() {
-  const [coin, setCoin] = useState(null);
+  // const [coin, setCoin] = useState(null); // null k case mein dynamic values are : {coins?.name} etc.
+  const [coin, setCoin] = useState({}); // & in this case values would be: {coins.name} etc.
+  const router = useRouter();
+  // console.log(router);
+
+  async function getCoinDetails() {
+    try {
+      const response = await fetch(
+        `https://api.coinlore.net/api/ticker/?id=${router.query.coin_id}`
+      );
+      const data = await response.json();
+      // console.log(data);
+      setCoin(data[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    if (router.query.coin_id) {
+      getCoinDetails();
+    }
+  }, [router.query.coin_id]);
 
   return (
     <div className="coin-detail">
       <div className="name-symbol">
-        <h1 className="name">coin-name</h1>
-        <h2 className="symbol">(coin-symbol)</h2>
+        <h1 className="name">{coin.name}</h1>
+        <h2 className="symbol">{coin.symbol}</h2>
       </div>
       <div className="market-description">
-        <p className="coin-rank">Rank: coin-symbol</p>
-        <p className="coin-price">Price: $coin-price</p>
-        <p className="coin-market-cap">Market Cap: $coin-market_cap_usd</p>
-        <p className="coin-total-supply">Total Supply: coin-tsupply</p>
-        <p className="coin-max-supply">Max Supply: coin-msupply</p>
+        <p className="coin-rank">Rank: {coin.rank}</p>
+        <p className="coin-price">Price: {coin.price_usd}</p>
+        <p className="coin-market-cap">Market Cap: {coin.market_cap_usd}</p>
+        <p className="coin-total-supply">Total Supply: {coin.tsupply}</p>
+        <p className="coin-max-supply">
+          Max Supply: {coin.msupply ? coin.msupply : "N/A"}
+        </p>
       </div>
     </div>
   );
